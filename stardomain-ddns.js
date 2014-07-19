471fd2d59b512580b7c20298f7a99206cebb90e0
lostman-github <lostman.worlds.end+github@gmail.com>
2014-07-19 18:12:16 +0900

    update DNS records when records is not matched

--- stardomain-ddns.js
+++ stardomain-ddns.js
@@ -10,13 +10,17 @@ var password = system.args[2];
 
 var fs = require('fs');
 if (!fs.isReadable(system.args[3])) {
-  console.log('can not read specified file.');
+  console.log('Could not read DNS records from specified file.');
+  console.log('exit.');
   phantom.exit();
 }
 
-var dnsRecords = fs.read(system.args[3]);
-console.log('update DNS records with');
+var dnsRecords = fs.read(system.args[3]).replace(/\s+$/, "");
+console.log('DNS records will be update with');
+console.log('----------');
 console.log(dnsRecords);
+console.log('----------');
+console.log('');
 
 var userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:30.0) Gecko/20100101 Firefox/30.0';
 
@@ -63,6 +67,26 @@ funcs.push(function () {
 });
 
 funcs.push(function () {
+  var current = page.evaluate(function() {
+    return document.getElementsByName('content')[0].value.replace(/\s+$/, "");
+  });
+
+  console.log('Current DNS records is');
+  console.log('----------');
+  console.log(current);
+  console.log('----------');
+  console.log('');
+
+  if (dnsRecords === current) {
+    console.log('DNS records is matched.');
+    console.log('Updating is not needed.');
+    console.log('exit.');
+    phantom.exit();
+  } else {
+    console.log('DNS records is not matched.');
+    console.log('Updating...');
+  }
+
   page.evaluate(function(d) {
     document.getElementsByName('content')[0].value = d;
     document.getElementsByName('action_user_dns_edittxt_conf')[0].click();
@@ -76,6 +100,7 @@ funcs.push(function () {
 });
 
 funcs.push(function () {
+  console.log('done.');
   phantom.exit();
 });
 
@@ -88,7 +113,8 @@ page.onLoadFinished = function () {
     var func = funcs.shift();
     func();
   } else {
-    console.log('queue is empty. exit.');
+    console.log('Function queue is empty.');
+    console.log('exit.');
     phantom.exit();
   }
 };
